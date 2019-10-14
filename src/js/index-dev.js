6fa463d11594208899ddd4a51a39e39aad9bd3e8
Phillip Troutman <philliptroutman21@gmail.com>
2019-10-14 12:09:02 -0400

    updated email

--- src/js/index-dev.js
+++ src/js/index-dev.js
@@ -11,7 +11,7 @@ const Form = new form(
       subject: 'OsLabs Contact Submission',
     };
 
-    const mailToLink = `mailto:opensourcelabs1@gmail.com?subject=${
+    const mailToLink = `mailto:hello@opensourcelabs.io?subject=${
       formData.subject
     }&body=${fields.reduce((acc, input) => {
       const subject = input.getElementsByTagName('label')[0].textContent;
